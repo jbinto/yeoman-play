cf79d436bc2cc1fb4127a051aec6bc9685a8a77b
Jesse Buchanan <jbinto@jbinto.ca>
2014-03-18 15:05:56 -0400

    Egghead.io #005: Add a method to the scope

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,6 +20,10 @@ app.controller('FirstCtrl', function ($scope, Data) {
 
 app.controller('SecondCtrl', function ($scope, Data) {
     $scope.data = Data;
+
+    $scope.reversedMessage = function(message) {
+      return message.split(" ").reverse().join(" ")
+    }
   }
 );
 
