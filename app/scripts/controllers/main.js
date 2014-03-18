349f4f878f662f1cef260259724ae8bfa3b55455
Jesse Buchanan <jbinto@jbinto.ca>
2014-03-18 14:51:00 -0400

    Egghead.io #004: Use factory to 'share' data (scope?) between controllers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,29 +1,35 @@
 'use strict';
 
-// Not quite sure about this invocation.
-// Egghead.io #02 just a single global function taking $scope.
-// Must clarify later.
-
-// ...And now it's later.
-// The app isn't working in production (after minification).
-// Probably, we need to use the Angular style below to register components
-// so they are minification friendly.
-//
-// http://docs.angularjs.org/error/$injector/unpr
-// https://github.com/angular-ui/ui-router/issues/785
+// Warning: Don't use global functions. They break minification.
 
 
-angular.module('projectfooApp')
-  .controller('MainCtrl', function ($scope) {
+// Warning: Don't use module(s,[]) here!
+// See "Creation versus Retrieval": 
+// http://docs.angularjs.org/guide/module
+
+var app = angular.module('projectfooApp');
+
+app.factory('Data', function() {
+  return { cssclass: 'alert alert-success' };
+});
+
+app.controller('FirstCtrl', function ($scope, Data) {
+    $scope.data = Data;
+  }
+);
+
+app.controller('SecondCtrl', function ($scope, Data) {
+    $scope.data = Data;
+  }
+);
+
+
+
+app.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  })
-  .controller('FirstCtrl', function ($scope) {
-    $scope.jshint = 'happy';
-  })
-  .controller('SecondCtrl', function ($scope) {
-    $scope.jshint = 'happy';
-  });
+  }
+);
